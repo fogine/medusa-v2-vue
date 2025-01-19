3c393e8a37e0895748e1da428f0f2755f21c1527
fogine <7884288+fogine@users.noreply.github.com>
2025-01-19 22:34:19 +0100

    cleanup

--- packages/core/tests/setup.ts
+++ packages/core/tests/setup.ts
@@ -1,23 +1,12 @@
 import { afterAll, afterEach, beforeAll } from 'vitest';
 import { server } from '../mocks/server';
 
-const allHandlers = server.listHandlers().map((i:any) => i.info.header).sort();
-// console.log(JSON.stringify(allHandlers, null, 4));
-const usedHandlers : string[] = [];
-
-server.events.on('request:match', (request) => {
-    usedHandlers.push(`${request.request.method} ${request.request.url}`);
-});
-
 beforeAll(() => {
     server.listen({onUnhandledRequest: 'error'});
 });
 
-// //  Close server after all tests
-afterAll(() => {
-    console.log(JSON.stringify(usedHandlers.sort(), null, 4));
-    server.close()
-});
+//  Close server after all tests
+afterAll(() => server.close());
 
-// // Reset handlers after each test `important for test isolation`
+// Reset handlers after each test `important for test isolation`
 afterEach(() => server.resetHandlers());
